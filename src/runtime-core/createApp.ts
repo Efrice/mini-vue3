f7b7118f554b63a2220f8cfe4fc6f39174392469
efrice <719431749@qq.com>
2023-08-03 18:22:16 +0800

    feat: createRenderer

--- src/runtime-core/createApp.ts
+++ src/runtime-core/createApp.ts
@@ -1,13 +1,14 @@
-import { render } from "./renderer"
 import { createVNode } from "./vnode"
 
-export function createApp(rootComponent){
-  return {
-    mount(rootSelector){
-      const rootContainer = document.querySelector(rootSelector)
-      const vnode = createVNode(rootComponent)
+export function createAppAPI(render){
+  return function createApp(rootComponent){
+    return {
+      mount(rootSelector){
+        const rootContainer = document.querySelector(rootSelector)
+        const vnode = createVNode(rootComponent)
 
-      render(vnode, rootContainer)
+        render(vnode, rootContainer)
+      }
     }
   }
 }
--- src/runtime-dom/index.ts
+++ src/runtime-dom/index.ts
@@ -1,3 +1,24 @@
+import { createRenderer } from '../runtime-core'
+import { getEvent, isArray, isOn, isString } from '../shared'
+
+function createElement(type){
+  return document.createElement(type)
+}
+
+function patchProp(el, key, vlaue){
+  if (isOn(key)) {
+    el.addEventListener(getEvent(key), vlaue)
+  }
+
+  if (isString(vlaue)) {
+    el.setAttribute(key, vlaue)
+  } else if (isArray(vlaue)) {
+    el.setAttribute(key, vlaue.join(' '))
+  }else if(vlaue === undefined || vlaue === null){
+    el.removeAttribute(key)
+  }
+}
+
 export function insert(el, parent, anchor = null){
   parent.insertBefore(el, anchor)
 }
@@ -8,3 +29,15 @@ export function remove(child) {
     parent.removeChild(child)
   }
 }
+
+const renderer: any = createRenderer({
+  createElement,
+  patchProp,
+  insert
+})
+
+export function createApp(...args){
+  return renderer.createApp(...args)
+}
+
+export * from '../runtime-core'
